83e8a48fc3e3e2608e781967d0000933e9d17e28
xtghjsa <xtghjsawork@gmail.com>
2024-04-28 01:57:39 +0300

    допил дней недели

--- api/repeatDate.go
+++ api/repeatDate.go
@@ -9,10 +9,16 @@ import (
 )
 
 func NextDate(now time.Time, date string, repeat string) (string, error) {
-	// Check year correctness in date
+	// Проверка на наличие даты повторения
 	if repeat == "" {
-		return "", errors.New("repeat can't be empty")
+		return "", errors.New("повтор не может быть пустым")
 	}
+	// Проверка на корректность формата даты
+	parsedDate, err := time.Parse("20060102", date)
+	if err != nil {
+		log.Println("неверный формат даты", err)
+	}
+	// Проверка на корректность года в дате
 	var yearCheckString string
 	for k, v := range date {
 		if k == 0 || k == 1 || k == 2 || k == 3 {
@@ -25,9 +31,9 @@ func NextDate(now time.Time, date string, repeat string) (string, error) {
 	}
 	yearMin := 0001
 	if yearCheckInt < yearMin {
-		return "", errors.New("year is out of range")
+		return "", errors.New("год в дате не может быть меньше 1")
 	}
-	// Check month correctness in date
+	// Проверка на корректность месяца в дате
 	var monthCheckString string
 	for k, v := range date {
 		if k == 4 || k == 5 {
@@ -41,9 +47,9 @@ func NextDate(now time.Time, date string, repeat string) (string, error) {
 	monthMax := 12
 	monthMin := 01
 	if monthCheckInt > monthMax || monthCheckInt < monthMin {
-		return "", errors.New("month is out of range")
+		return "", errors.New("месяц в дате не может быть больше 12 или меньше 1")
 	}
-	// Check day correctness in date
+	// Проверка на корректность дня в дате
 	var dayCheckString string
 	for k, v := range date {
 		if k == 6 || k == 7 {
@@ -57,33 +63,27 @@ func NextDate(now time.Time, date string, repeat string) (string, error) {
 	dayMax := 31
 	dayMin := 01
 	if dayCheckInt > dayMax || dayCheckInt < dayMin {
-		return "", errors.New("day is out of range")
+		return "", errors.New("дни в дате не может быть больше 31 или меньше 1")
 	}
-	// Check date correctness
-	parsedDate, err := time.Parse("20060102", date)
-	if err != nil {
-		log.Println("wrong date format", err)
-	}
+
 	rules := strings.Split(repeat, " ")
-	// Check repeat symbol correctness
+	// Проверка на корректность формата задаваемого повтора
 	if rules[0] != "d" && rules[0] != "w" && rules[0] != "m" && rules[0] != "y" {
-		return "", errors.New("wrong repeat symbol")
+		return "", errors.New("неправильный формат повтора")
 	}
+	// Проверка на наличие выбранных дней
 	if rules[0] == "d" && len(rules) == 1 {
-		return "", errors.New("days can't be empty")
+		return "", errors.New("количество выбранных дней не может быть пустым")
 	}
 	switch rules[0] {
-	// day
+	// дни
 	case "d":
-		if len(rules) == 1 {
-			return "", errors.New("days amount can't be empty")
-		}
 		days, err := strconv.Atoi(rules[1])
 		if err != nil {
-			log.Println("wrong days format or days are not specified", err)
+			return "", err
 		}
-		if days > 400 || days <= 0 {
-			return "", errors.New("days can't be more than 400 and less than 0")
+		if days > 400 || days < 1 {
+			return "", errors.New("дней не может быть больше 400 и меньше 1")
 		}
 		var caseDResult string
 		if parsedDate.After(now) {
@@ -93,13 +93,45 @@ func NextDate(now time.Time, date string, repeat string) (string, error) {
 			caseDResult = i.AddDate(0, 0, days).Format("20060102")
 		}
 		return caseDResult, nil
-		// week
+		// дни недели
 	case "w":
-		return "", errors.New("not implemented, WIP")
-		// month
+		// Проверка на наличие выбранных дней недели
+		if len(rules) == 1 {
+			return "", errors.New("количество выбранных дней недели не может быть пустым")
+		}
+
+		chosenWeekdaysStr := strings.Split(rules[1], ",")
+		chosenWeekdaysInt := make([]int, 0, 7)
+		weekdaysMap := map[int]time.Weekday{1: time.Monday, 2: time.Tuesday, 3: time.Wednesday, 4: time.Thursday, 5: time.Friday, 6: time.Saturday, 7: time.Sunday}
+		for _, v := range chosenWeekdaysStr {
+			weekday, err := strconv.Atoi(v)
+			if err != nil {
+				return "", err
+			}
+			if weekday > 7 || weekday < 1 {
+				return "", errors.New("день недели не может быть больше 7 или меньше 1")
+			}
+			chosenWeekdaysInt = append(chosenWeekdaysInt, weekday)
+		}
+
+		nowUnix := now.Unix()
+		var day int64 = 86400
+		for i := 0; i < 999; i++ {
+			nowUnix += day
+			for _, v := range chosenWeekdaysInt {
+				caseWResult := time.Unix(nowUnix, 0)
+				if caseWResult.Weekday() == weekdaysMap[v] {
+					return caseWResult.Format("20060102"), nil
+				}
+			}
+		}
+		return "", err
+
+		// дни месяца
 	case "m":
 		return "", errors.New("not implemented, WIP")
-		// year
+
+		// год
 	case "y":
 		var caseYResult string
 		if parsedDate.After(now) {
@@ -110,5 +142,5 @@ func NextDate(now time.Time, date string, repeat string) (string, error) {
 		}
 		return caseYResult, nil
 	}
-	return "", errors.New("wrong repeat format")
+	return "", errors.New("неправильный формат повтора")
 }
--- tests/settings.go
+++ tests/settings.go
@@ -2,6 +2,6 @@ package tests
 
 var Port = 7540
 var DBFile = "E:\\Dev\\go_final_project\\database\\scheduler.db"
-var FullNextDate = false
+var FullNextDate = true
 var Search = false
 var Token = ``
